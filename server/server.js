dfa52611772f0bee2de695c695141eff4850a5b3
Zach Coley <zachcoley@users.noreply.github.com>
2022-04-05 16:06:47 +1200

    Added api routes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // import dependencies
 const express = require('express');
+const path = require('path');
 
 // create an instance of express
 const app = express();
@@ -7,9 +8,28 @@ const app = express();
 // set the server port to either the port set by heroku or 3001
 const port = process.env.PORT || 3001;
 
-// create a test response on the api route
-app.get('/api', (req, res) => {
-  res.end('Hello World');
+// route to get all scp subjects
+app.get('/api/scps', (req, res) => {
+  // ...
+});
+
+// route to get a single scp subject by its item number (id) e.g: /api/scps/002
+app.get('/api/scps/:item', (req, res) => {
+  const { item } = req.params;
+  console.log(`SCP-${item}`);
+  // ...
+});
+
+// route to get an image by its filename from the /server/images directory
+app.get('/api/images/:filename', (req, res) => {
+  const { filename } = req.params;
+  console.log(filename);
+  res.sendFile(path.resolve(__dirname, './images', filename));
+});
+
+// any unhandled routes will return the react application
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../react-client-app/build', 'index.html'));
 });
 
 // tell the server to begin listening on the selected port
