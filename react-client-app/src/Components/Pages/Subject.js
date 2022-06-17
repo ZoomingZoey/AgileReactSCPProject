0afc5e2621bff332bfcd6265fdaa448a39b4653d
Zach Coley <zachcoley@users.noreply.github.com>
2022-06-17 14:09:44 +1200

    Implemented markdown processing and made small changes to the Subject.js page

--- react-client-app/src/Components/Pages/Subject.js
+++ react-client-app/src/Components/Pages/Subject.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { getMediaPath, removeItemStringPrefix } from "../../lib/helpers";
 import { useParams } from "react-router-dom";
 import Footer from "../Footer";
+import MarkdownIt from "markdown-it";
 
 const Subject = () => {
 
@@ -14,6 +15,8 @@ const Subject = () => {
       .then(data => setSubject(data));
   }, []);
 
+  const md = new MarkdownIt();
+
   return (
     <div>
       {subject !== null &&
@@ -24,26 +27,29 @@ const Subject = () => {
           <div className="cards_container">
             <div className="cards_wrapper">
               <ul className="cards_items">
-          <h3><strong>Item#: </strong>{subject.item}</h3>
-          <h2><strong>Object Class: </strong>{subject.object_class}</h2>
-          <br/>
-          <h2><i>Special Containment Procedures: </i></h2>
-          <p>{subject.containment_procedures ? subject.containment_procedures : 'No procedures provided'}</p>
-          <br></br>
-          <p><strong><i>Description:</i></strong>  {subject.description}</p>
-          <div>
-            <br></br>
-            <br></br>
-            {subject.images &&
-              subject.images.map(image => (
-                <figure key={image._id}>
-                  <img src={getMediaPath(image.url)} title={subject.item} alt={image.caption}/>
-                  <figcaption><i>{image.caption}</i></figcaption>
-                </figure>
-              ))
-            }
-             </div>
-             </ul>
+                <h2><strong>Item#: </strong>{subject.item}</h2>
+                <h2><strong>Object Class: </strong>{subject.object_class}</h2>
+                <br/>
+                <h2><i>Special Containment Procedures:</i></h2>
+                <p>{subject.containment_procedures ? subject.containment_procedures : 'No procedures provided'}</p>
+                <div>
+                  {subject.images &&
+                    <>
+                      <br></br>
+                      <br></br>
+                      {subject.images.map(image => (
+                        <figure key={image._id} className='subject-image'>
+                          <img className="image-fluid" src={getMediaPath(image.url)} title={subject.item} alt={image.caption}/>
+                          <figcaption><i>{image.caption}</i></figcaption>
+                        </figure>
+                      ))}
+                    </>
+                  }
+                </div>
+                <br/>
+                <h2><strong><i>Description:</i></strong></h2>
+                <section dangerouslySetInnerHTML={{ __html: md.render(subject.description)}}></section>
+              </ul>
             </div>
           </div>
         </div>
